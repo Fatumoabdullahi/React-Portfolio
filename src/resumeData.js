9235f305fd5a2bd9475a5f1358cd59a83d359862
Fatumoabdullahi <95045817+Fatumoabdullahi@users.noreply.github.com>
2022-07-12 13:33:32 +0100

    portfolio done

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,106 +1,112 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/rbhatia46",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
-    "education":[
-      {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+  imagebaseurl: "https://github.com/Fatumoabdullahi/",
+  name: "Fatumo Abdullahi",
+  role: "Full Stack Developer",
+  linkedinId: "www.linkedin.com/in/fatumo-abdullahi-11a663199",
+  skypeid: "Your skypeid",
+  roleDescription: "with a passion for exploring new technologies.",
+
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "http://linkedin.com/in/fatumo-abdullahi-11a663199",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/Fatumoabdullahi",
+      className: "fa fa-github",
+    },
+    {
+      name: "skype",
+      url: "https://twitter.com/FatimaA31380895",
+      className: "fa fa-twitter",
+    },
+  ],
+  aboutme:
+    "I'm an ambitious full stack developer with a background in linguistics and teaching, hoping to kick start my career in software development. I am looking for a role in a diverse company with the opportunity to work with the latest technologies on challenging projects. I am a confident and ambitious learner, a keen individual with an interest in tech, and a risk taker who is always up for a challenge. If I was to define myself I would say that I am a friendly individual who enjoys nature in all its aspects, a good book and the odd coffee from time to time. ",
+  address: "Birmingham, UK",
+  website: "https://github.com/Fatumoabdullahi",
+  education: [
+    {
+      UniversityName: "University of Birmingham",
+      specialization: "Coding Bootcamp",
+      MonthOfPassing: "Jun",
+      YearOfPassing: "2022",
+      Achievements:
+        "A comprehensive study of front-end and back-end technologies and languages including: HTML, CSS, Bootstrap, React.js, Javascript, MySQL, Postman, Dbeaver.",
+    },
+    {
+      UniversityName: "Coventry University",
+      specialization: "MA English Language Teaching and Applied Linguistics",
+      MonthOfPassing: "Sep",
+      YearOfPassing: "2020",
+      Achievements:
+        "An innovative study of numerous teaching approaches and a pathway into English for Academic Purposes and Linguistics. Modules include: Analysing Written and Spoken Discourse, Computer Assisted Language Learning, Developing Language Teaching Materials, Understanding Academic English.",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Tradewind Recruitment",
+      specialization: "Teaching Assistant",
+      MonthOfLeaving: "Current",
+      YearOfLeaving: "",
+      Achievements:
+        "Working as a Teaching Assistant has allowed me to establish outstanding multitasking skills to ensure I complete tasks efficiently and meet marking deadlines, attention to detail and ensuring my students reach their highest potential and excellent time management skills to ensure lesson plans are followed meticulously.",
+    },
+    {
+      CompanyName: "Telford College",
+      specialization: "Functional Skills Tutor",
+      MonthOfLeaving: "May",
+      YearOfLeaving: "2018",
+      Achievements:
+        "Using a communicative approach to support learners in order to enhance their speaking, listening, reading and writing skills in General English at levels 1 and 2 for Functional Skills. Adapting teching material depending on learners' language needs.",
+    },
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "Reactjs",
+    },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+  ],
+};
+
+export default resumeData;
