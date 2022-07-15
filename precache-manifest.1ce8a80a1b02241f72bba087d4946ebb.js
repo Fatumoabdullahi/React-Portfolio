b5e57be5ac5846f8575bed5697f8b46127156081
Fatumoabdullahi <95045817+Fatumoabdullahi@users.noreply.github.com>
2022-07-15 21:09:08 +0100

    Updates

---
+++ precache-manifest.1ce8a80a1b02241f72bba087d4946ebb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d2dd06946b8c6398f94",
+    "url": "/React-Portfolio/static/css/main.30c318c7.chunk.css"
+  },
+  {
+    "revision": "7d2dd06946b8c6398f94",
+    "url": "/React-Portfolio/static/js/main.7d2dd069.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/React-Portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "804d233a10add83efa8d",
+    "url": "/React-Portfolio/static/js/runtime~main.804d233a.js"
+  },
+  {
+    "revision": "96e59943ba7c36457fc2890c2bc9ead7",
+    "url": "/React-Portfolio/index.html"
+  }
+];
